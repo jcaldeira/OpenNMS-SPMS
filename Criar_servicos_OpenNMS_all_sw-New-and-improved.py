1140e7453b541834a98a95e4e82ff3f9689b9472
João Caldeira <joao.caldeira.ext@spms.min-saude.pt>
2021-05-10 17:08:03 +0100

    Scripts complete
    -Create Services: It works with a copy of the source excel if it's in use (Windows only [yet])
    -Create Services: List with special characters increased
    -Create Services: Striped all camps
    -New environment file format created
    -SW infos: completed

--- Criar_servicos_OpenNMS_all_sw-New-and-improved.py
+++ Criar_servicos_OpenNMS_all_sw-New-and-improved.py
@@ -1,33 +1,52 @@
-import ipaddress
-import sys
+"""
+https://stackoverflow.com/questions/56770093/read-from-excel-file-that-is-open-in-python
+"""
+
 import datetime
+import ipaddress
+import os
+import subprocess
+import sys
+
 import openpyxl
 
 
 def main():
-    excel = "C:\\Users\\joao.caldeira.ext\\SPMS - Serviços Partilhados do Ministério da Saúde, EPE\\SPMS DSI RDIS - RIS Corporate - RIS2020 Cadastro\\RIS2020 - Cadastro.xlsx"
-    chars_a_remover = ['á','à','ã','â','é','è','ê','í','ó','õ','ô','ú','û','ù','ç','s/n']
-    chars_a_inserir = ['a','a','a','a','e','e','e','i','o','o','o','u','u','u','c','']
+    excel = r"C:\Users\joao.caldeira.ext\SPMS - Serviços Partilhados do Ministério da Saúde, EPE\SPMS DSI RDIS - RIS Corporate - RIS2020 Cadastro\RIS2020 - Cadastro.xlsx"
+    excelFileName = os.path.basename(excel)
+    pathToExcelFile = os.path.dirname(excel)
+
+    chars_a_remover = ['á','Á','à','À','ã','Ã','â','Â','é','É','è','È','ê','Ê','í','Í','ó','Ó','õ','Õ','ô','Ô','ú','Ú','û','Û','ù','Ù','ç','s/n']
+    chars_a_inserir = ['a','A','a','A','a','A','a','A','e','E','e','E','e','E','i','I','o','O','o','O','o','O','u','U','u','U','u','U','c','']
 
     try:
-        wb = openpyxl.load_workbook(excel, data_only=True)
-    except Exception as e:
-        sys.exit(f"{e}\nImpossible to open excel file: {excel}")
-    else:
-        sheet = wb["RIS2020"]
+        wb = openpyxl.load_workbook(excel, data_only=True, read_only=True)
+        delTempFileFlag = False
+    except PermissionError as e:
+        print(f"File is open, so cannot be accessed:\n{e.filename}\n")
+        createTempFile(pathToExcelFile, os.getcwd(), excelFileName)
+        print("Temporary copy of file created to work with\n")
+        wb = openpyxl.load_workbook(excelFileName, data_only=True, read_only=True)
+        delTempFileFlag = True
 
+    except FileNotFoundError as e:
+        sys.exit(f"File not found: {e.filename}")
+
+
+    sheet = wb["RIS2020"]
     counter = 0
-    excluded_ids = ['0417']
-    excluded_ips = ['10.34.6.241', '10.11.20.241', '10.13.200.241', '10.13.200.242']
+    excluded_ids = []
+    excluded_ips = []
     for row in sheet.iter_rows(min_row=3, values_only=True):
         if type(row[26]) == datetime.datetime:
         # if row[26] is not None:
             data_presi = row[26].date()
             ip = str(row[24])
             hostname = str(row[23])
-            site_id = hostname[0:4]
-            if validate_ip(ip) and validate_date_inter(data_presi) and site_id not in excluded_ids and ip not in excluded_ips:
+            site_id = hostname[:4]
+            # if validate_ip(ip) and validate_date_inter(data_presi) and site_id not in excluded_ids and ip not in excluded_ips:
             # if validate_ip(ip) and hostname == '0073-B01-SW01':
+            if validate_ip(ip) and site_id == '0287':
                 if str(row[0]) == site_id:
                     entidade, morada, cp, localidade, latitude, longitude, requisition, modelo, serial, node_id = site_info(chars_a_remover, chars_a_inserir, row)
 
@@ -50,36 +69,36 @@ def main():
                     counter += 1
 
                 else:
-                    print(f"IDs do not match {site_id} (on switch name)")
+                    print(f"IDs do not match {site_id} (on switch name)\n")
 
-            else:
-                continue
-                print(f"Invalid IP: {ip}")
+    wb.close()
+    if delTempFileFlag == True:
+        os.remove(excelFileName)
 
     print(f'Generated {counter} configurations')
 
 
 def site_info(chars_a_remover, chars_a_inserir, row):
-    entidade = str(row[1]).replace(" ","_")
-    morada = str(row[4])
-    cp = str(row[5])
-    localidade = str(row[6])
-    latitude = str(row[7])
-    longitude = str(row[8])
-    requisition = str(row[28])
-    modelo = str(row[20])
-    serial = str(row[21])
-    node_id = str(row[27])
+    entidade = str(row[1]).replace(" ","_").strip()
+    morada = str(row[4]).strip()
+    cp = str(row[5]).strip()
+    localidade = str(row[6]).strip()
+    latitude = str(row[7]).strip()
+    longitude = str(row[8]).strip()
+    requisition = str(row[28]).strip()
+    modelo = str(row[20]).strip()
+    serial = str(row[21]).strip()
+    node_id = str(row[27]).strip()
     for i in range(0,len(chars_a_remover)):
-        morada = morada.replace(chars_a_remover[i],chars_a_inserir[i])
-        localidade = localidade.replace(chars_a_remover[i], chars_a_inserir[i])
+        morada = morada.lower().replace(chars_a_remover[i],chars_a_inserir[i]).strip().capitalize()
+        localidade = localidade.lower().replace(chars_a_remover[i], chars_a_inserir[i]).strip().capitalize()
     return entidade, morada, cp, localidade, latitude, longitude, requisition, modelo, serial, node_id
 
 
 
 def validate_date_inter(date):
-    low_date_lim = datetime.date(2021, 4, 27)
-    high_date_lim = datetime.date(2021, 4, 29)
+    low_date_lim = datetime.date(2021, 5, 4)
+    high_date_lim = datetime.date(2021, 5, 5)
     return low_date_lim <= date <= high_date_lim
 
 
@@ -94,5 +113,22 @@ def validate_ip(ip):
 
 
 
+def cls():
+    if sys.platform in ("linux", "darwin"):
+        subprocess.run(["clear"])
+    elif sys.platform == "win32":
+        subprocess.run(["cls"], shell=True)
+
+
+
+def createTempFile(pathToExcelFile, cwd, excelFileName):
+    if sys.platform in ("linux", "darwin"):
+        sys.exit(f"Copy mechanism of in use files not implemented in non Windows platforms")
+    elif sys.platform == "win32":
+        subprocess.run(["robocopy", pathToExcelFile, cwd, excelFileName], shell=True, stdout=subprocess.DEVNULL)
+
+
+
 if __name__ == "__main__":
+    cls()
     main()
---
+++ gather_SW_infos.py
@@ -0,0 +1,211 @@
+"""
+Tem de ser executado no WLS
+"""
+
+import concurrent.futures
+import datetime
+import ipaddress
+import logging
+import os
+import re
+import subprocess
+import sys
+import time
+
+import netmiko
+import openpyxl
+from netmiko.ssh_exception import (AuthenticationException,
+                                   NetMikoTimeoutException)
+from paramiko.ssh_exception import SSHException
+
+# ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ Logging ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+main_logger = logging.getLogger(__name__) # criar um logger específico deste módulo
+main_logger.setLevel(logging.DEBUG) # definir o nível de verbosidade do logger
+
+file_formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s') # criar o formato dos logs a aplicar no file_formatter
+steam_formatter = logging.Formatter('%(message)s') # criar o formato dos logs a aplicar no stream_handler
+
+file_handler = logging.FileHandler(f'{os.path.splitext(os.path.basename(__file__))[0]}.log') # criar ficheiro de logs
+file_handler.setLevel(logging.DEBUG) # definir o nível de verbosidade do file_handler
+file_handler.setFormatter(file_formatter) # aplicar o formato de log anteriormente criado
+
+stream_handler = logging.StreamHandler() # criar stream_handler que serve para mostrar logs noutros canais
+stream_handler.setLevel(logging.INFO) # definir o nível de verbosidade do stream_handler
+stream_handler.setFormatter(steam_formatter) # aplicar o formato de log anteriormente criado
+
+main_logger.addHandler(file_handler) # aplicar file_handler ao logger
+main_logger.addHandler(stream_handler) # aplicar stream_handler ao logger
+
+# logging.basicConfig(filename='netmiko.log', level=logging.DEBUG)
+# logger = logging.getLogger("netmiko")
+# ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ End Logging ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+
+def validate_ip(ip):
+    try:
+        ipaddress.ip_address(ip)
+    except:
+        return False
+    else:
+        return True
+
+
+
+def validate_date_inter(date):
+    low_date_lim = datetime.date(2021, 5, 4)
+    high_date_lim = datetime.date(2021, 5, 5)
+    return low_date_lim <= date <= high_date_lim
+
+
+
+def cls():
+    if sys.platform in ("linux", "darwin"):
+        subprocess.run(["clear"])
+    elif sys.platform == "win32":
+        subprocess.run(["cls"], shell=True)
+
+
+
+def createTempFile(pathToExcelFile, cwd, excelFileName):
+    if sys.platform in ("linux", "darwin"):
+        sys.exit(f"Copy mechanism of in use files not implemented in non Windows platforms")
+    elif sys.platform == "win32":
+        subprocess.run(["robocopy", pathToExcelFile, cwd, excelFileName], shell=True, stdout=subprocess.DEVNULL)
+
+
+
+def env_exec():
+    # excel = r"C:\Users\joao.caldeira.ext\SPMS - Serviços Partilhados do Ministério da Saúde, EPE\SPMS DSI RDIS - RIS Corporate - RIS2020 Cadastro\RIS2020 - Cadastro.xlsx"
+    excel = "/mnt/c/Users/joao.caldeira.ext/SPMS - Serviços Partilhados do Ministério da Saúde, EPE/SPMS DSI RDIS - RIS Corporate - RIS2020 Cadastro/RIS2020 - Cadastro.xlsx"
+
+    excelFileName = os.path.basename(excel)
+    pathToExcelFile = os.path.dirname(excel)
+
+    username = 'jcaldeira'
+    password = os.getenv('PWD_TACACS_RIS_SPMS')
+
+    try:
+        wb = openpyxl.load_workbook(excel, data_only=True, read_only=True)
+        delTempFileFlag = False
+    except PermissionError as e:
+        print(f"File is open, so cannot be accessed:\n{e.filename}\n")
+        createTempFile(pathToExcelFile, os.getcwd(), excelFileName)
+        print("Temporary copy of file created to work with\n")
+        wb = openpyxl.load_workbook(excelFileName, data_only=True, read_only=True)
+        delTempFileFlag = True
+    except FileNotFoundError as e:
+        sys.exit(f"File not found: {e.filename}")
+
+    sheet = wb["RIS2020"]
+    device_list = []
+    excluded_ids = []
+    excluded_ips = []
+
+    for row in sheet.iter_rows(min_row=3, values_only=True):
+        if type(row[26]) == datetime.datetime:
+        # if row[26] is not None:
+            data_presi = row[26].date()
+            ip = str(row[24])
+            hostname = str(row[23])
+            site_id = hostname[:4]
+            # if validate_ip(ip) and validate_date_inter(data_presi) and site_id not in excluded_ids and ip not in excluded_ips:
+            # if validate_ip(ip) and hostname == '0073-B01-SW01':
+            if validate_ip(ip) and site_id == '0119':
+                if str(row[0]) == site_id:
+                    equipment = {
+                        'device_type': 'cisco_ios',
+                        'ip': ip,
+                        'username': username,
+                        'password': password,
+                        'secret': site_id,
+                        'ssh_config_file': '~/.ssh/config'
+                    }
+
+                    device_list.append(equipment)
+
+
+    main_logger.debug(f'device_list: {device_list}')
+
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(connect_and_commands, device_list)
+
+    wb.close()
+    if delTempFileFlag == True:
+        os.remove(excelFileName)
+
+    return len(device_list)
+
+
+
+def connect_and_commands(equipment):
+    main_logger.info(f"Accessing: {equipment['secret']} ({equipment['ip']})")
+    try:
+        with netmiko.ConnectHandler(**equipment) as connection:
+            command_string = 'show version'
+            output = connection.send_command(command_string = command_string)
+            hostname = connection.find_prompt()
+
+    except NetMikoTimeoutException:
+        main_logger.info(f"Timeout exception on {equipment['secret']} ({equipment['ip']})")
+
+    except AuthenticationException:
+        main_logger.info(f"Authentication failed on {equipment['secret']} ({equipment['ip']})")
+
+    except SSHException:
+        main_logger.info(f"Error reading SSH protocol banner on {equipment['secret']} ({equipment['ip']})")
+
+    except:
+        main_logger.exception(f"An error has occurred on {equipment['secret']} ({equipment['ip']})")
+
+    else:
+        hostname, model, serial_number, ios = equip_info(output, hostname)
+
+        write_output(equipment, model, serial_number, ios, hostname)
+
+
+
+def equip_info(output, hostname):
+    re_model = r'(Model number.*)'
+    re_model_res = re.search(re_model, output)
+    re_model_res = re_model_res.group()
+    model = re_model_res.partition(":")[2].strip()
+
+    re_serial_number = r'(System serial number.*)'
+    re_serial_number_res = re.search(re_serial_number, output)
+    re_serial_number_res = re_serial_number_res.group()
+    serial_number = re_serial_number_res.partition(":")[2].strip()
+
+    re_ios = r'(System image file is.*)'
+    re_ios_res = re.search(re_ios, output)
+    re_ios_res = re_ios_res.group()
+    pos_ios = re_ios_res.partition(":")[2].find("/", 1)
+    ios = re_ios_res.partition(":")[2][pos_ios+1:-1].replace("/", "").strip()
+
+    hostname = hostname[:-1].strip()
+
+    return hostname,model,serial_number,ios
+
+
+
+def write_output(equipment, model, serial_number, ios, hostname):
+    with open(os.path.join(os.getcwd(), 'output.txt'), 'at') as f:
+        f.write(f"{equipment['secret']} ({equipment['ip']}): {model}\t{serial_number}\t{ios}\t{hostname}\n")
+
+
+
+if __name__ == '__main__':
+    cls()
+    time_start = datetime.datetime.now()
+    time_start = time_start.strftime('%H:%M:%S %d/%m/%Y')
+    perf_counter_start = time.perf_counter()
+
+    num_equips = env_exec()
+
+    perf_counter_stop = time.perf_counter()
+    time_stop = datetime.datetime.now()
+    time_stop = time_stop.strftime('%H:%M:%S %d/%m/%Y')
+
+    main_logger.info(f'Number of equipments: {num_equips}')
+    main_logger.info(f'Finished in {(perf_counter_stop - perf_counter_start):.3f} second(s)')
+    main_logger.info(f'Started at {time_start} and finished at {time_stop}\n')
